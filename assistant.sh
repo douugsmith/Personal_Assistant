3805a21cdec2fc1218568e6325e0656f5d06a957
douugsmith <39501976+douugsmith@users.noreply.github.com>
2018-06-20 10:11:05 -0700

    Add files via upload

---
+++ assistant.sh
@@ -0,0 +1,27 @@
+echo "Hello, Doug! I am Samantha, your personal assistant." 
+printf "\n"
+
+echo "Current date and time is..."
+printf "\n"
+
+date
+printf "\n"
+
+echo "The calendar is..."
+printf "\n"
+
+cal
+printf "\n"
+
+#add ten tips
+
+echo "1. Make sure your computer is charged before the next class."
+echo "2. Open a new Terminal document."
+echo "3. Type cd DIGHUM101 into the Terminal."
+echo "4. Type git pull into Terminal."
+echo "5. After the command is run, type jupyter notebook to launch the notebook"echo "6. Once notebook is opened, click on homework folder."
+echo "7. Click on DIGHUM101_Homework2.ipynb"
+echo "8. Read carefully though assignment before beginning."
+echo "9. Follow the steps under the Example Workflow section to begin your personal assistant assignment."
+echo "10. Begin typing commands for your assistant to give."
+
